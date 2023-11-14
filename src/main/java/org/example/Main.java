aec6ad19e7fe726bacc846a905d163d4cb117d67
karthik4703 <dck150303@gmail.com>
2023-11-14 18:40:35 +0530

    Github webhook

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
-    public static void main(String[] args) {
+    public  static void main(String[] args) {
         logger.info("Calculator Begins!!");
         //some changes
         Scanner reader = new Scanner(System.in);
